1c6aa684774b7c6652e4a315126cdb75fcc223cb
jobayermojumder <jobayer.pro@gmail.com>
2018-08-24 23:16:02 +0600

    add login html page

--- cadmin/views.py
+++ cadmin/views.py
@@ -12,14 +12,17 @@ def index(request):
 
 
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse('cadmin_index'))
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('cadmin_index'))
+        else:
+            return render(request, 'cadmin/login.html', {'message': 'User not found!'})
     else:
-        return render(request, 'cadmin/login.html', {'message': 'User not found!'})
+        return render(request, 'cadmin/login.html', {'message': "Please Login!"})
 
 
 def logout_view(request):
